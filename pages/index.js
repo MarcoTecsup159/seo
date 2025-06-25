5b20b2462fc10eb6e0b9c63438dc136b5fd079ce
MarcoTecsup159 <marco.rios.c@tecsup.edu.pe>
2025-06-24 22:02:22 -0500

    correcion en la verificacion

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import dynamic from "next/dynamic";
+import Head from "next/head";
 
 const DynamicComponent = dynamic(() => import("../components/LargeComponent"), { ssr: false });
 
 export default function Home() {
   return (
     <>
-    <head>
-      <meta name="google-site-verification" content="POr69ZX42X9JitHpNjlKIGmJapGJmXoIKAWkzRp7Puc" />
-    </head>
+      <Head>
+        <meta name="google-site-verification" content="POr69ZX42X9JitHpNjlKIGmJapGJmXoIKAWkzRp7Puc" />
+      </Head>
       <h1>Bienvenido</h1>
       <DynamicComponent />
     </>
