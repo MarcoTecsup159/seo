09aa3842ef56f85b2eb2b3232efb8f1a752b21f7
MarcoTecsup159 <marco.rios.c@tecsup.edu.pe>
2025-06-24 22:51:04 -0500

    fix: serve sitemap with res.send

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -1,24 +1,21 @@
 export default async function handler(req, res) {
-  //simular obtener datos de tu base de datos o CMS
   const pages = [
-    { url: '', lastmod: '2024-06-24' },  // Home
-    { url: 'blog', lastmod: '2024-06-20' },  // Blog
-    { url: 'contacto', lastmod: '2024-06-22' },  // Contacto
+    { url: '', lastmod: '2024-06-24' },
+    { url: 'blog', lastmod: '2024-06-20' },
+    { url: 'contacto', lastmod: '2024-06-22' },
   ];
 
   const baseUrl = 'https://seo-nu-one.vercel.app';
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${pages.map(page => `
-      <url>
-        <loc>${baseUrl}/${page.url}</loc>
-        <lastmod>${page.lastmod}</lastmod>
-      </url>
-    `).join('')}
-  </urlset>`;
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${pages.map(page => `
+  <url>
+    <loc>${baseUrl}/${page.url}</loc>
+    <lastmod>${page.lastmod}</lastmod>
+  </url>`).join('')}
+</urlset>`;
 
   res.setHeader('Content-Type', 'text/xml');
-  res.write(sitemap);
-  res.end();
+  res.status(200).send(sitemap);
 }
